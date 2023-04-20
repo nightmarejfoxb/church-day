ef1bfdabc73d6c3d2f3f3c28ae838299cf986880
nightmarejfoxb <118293178+nightmarejfoxb@users.noreply.github.com>
2023-04-20 09:19:43 -0700

    update

---
+++ Codes/game code/Ui/HealthBarUI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HealthBarUI : MonoBehaviour
+{
+    [SerializeField] private character character;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private Image healthBarFill;
+
+    private void OnEnable()
+    {
+        character.onTakeDamage += UpdateHealthBar;
+        character.onHeal += UpdateHealthBar;
+    }
+    private void OnDisable()
+    {
+        character.onTakeDamage -= UpdateHealthBar;
+        character.onHeal -= UpdateHealthBar;
+    }
+
+    private void Start()
+    {
+        SetNameText(character. DisplayName);
+    }
+    void SetNameText(string text)
+    {
+        nameText.text = text;
+    }
+
+    void UpdateHealthBar()
+    {
+        float healthPercent = (float)character.CurHp / (float)character.MaxHp;
+        healthBarFill.fillAmount = healthPercent;
+    }
+
+
+    //Testing the HP bar
+    private void Update()
+    {
+        UpdateHealthBar();
+    }
+}
